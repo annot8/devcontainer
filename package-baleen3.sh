6f28e2c875c8df0932d18780e96e4d5a7cd84cee
Chris Flatley <chris@committed.io>
2021-02-22 11:03:32 +0000

    Copy readme to baleen 3 package

--- package-baleen3.sh
+++ package-baleen3.sh
@@ -8,6 +8,11 @@ rm -rf baleen3-package/components/*-javadoc.jar
 cp -r dstl-baleen3/pipelines baleen3-package/pipelines
 cp -r dstl-baleen3/templates baleen3-package/templates
 cp -r dstl-baleen3/target/baleen-*.jar baleen3-package/
+cp -r dstl-baleen3/LICENSE.txt baleen3-package/
+cp -r dstl-baleen3/THIRD-PARTY.txt baleen3-package/
+cp -r dstl-baleen3/*.svg baleen3-package/
+cp -r dstl-baleen3/GETTING-STARTED.md baleen3-package/README.md
+
 echo "java -jar baleen-3.*.jar" > baleen3-package/run.sh
 echo "java -jar baleen-3.*.jar" > baleen3-package/run.bat
 chmod +x baleen3-package/run.sh
